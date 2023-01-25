b9b46bf82a491c1f352923c6dd29e088eb2d6550
alberto <59926312+alovba1@users.noreply.github.com>
2023-01-24 19:14:57 -0500

    ok

--- src/test/java/test/Base.java
+++ src/test/java/test/Base.java
@@ -30,7 +30,7 @@ import org.junit.internal.AssumptionViolatedException;
 class Base {
 
     protected void run() {
-        //.out.println("MENSAJE DE INICIO");
+        System.out.println("MENSAJE DE INICIO");
         double r = Math.random();
         //System.out.println("Mensaje OK OK");
         if (r < 0.1) {
